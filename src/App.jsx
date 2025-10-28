ff35f3ef05203f4ed7244cbe05f6b926f140039f
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-10-28 20:25:42 +0530

    added resume and some skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
